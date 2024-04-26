18164b468043359186463b9c9d5584bbe27dfeac
saulsneider7 <saulsneider@hotmail.com>
2024-04-26 16:46:11 -0500

    while

--- sesion5.py
+++ sesion5.py
@@ -5,3 +5,31 @@ class Game:
     aliens = []
     rockets = []
     lost = False
+
+    def __init__(self, width, height):
+        pygame.init()
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((width, height))
+        self.clock = pygame.time.Clock()
+        done = False
+
+        hero = Hero(self, width / 2, height - 20 )
+        generator = Generator(self)
+        rocket = None
+
+        while not done:
+            if len(self.aliens) == 44:
+                self.displayText("YOU WIN!")
+            
+            pressed = pygame.key.get_pressed()
+            if pressed[pygame.K_LEFT]:
+                hero.x -= 2 if hero.x > 20 else 0
+            elif pressed[pygame.K_RIGHT]:
+                hero.x += 2 if hero.x < width - 20 else 0
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and not self.lost:
+                    self.rockets.append(Rocket(self, hero.x, hero.y))
+            
