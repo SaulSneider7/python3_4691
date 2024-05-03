5327c3b10abf504c886f54cd71558a1ef4de56c2
saulsneider7 <saulsneider@hotmail.com>
2024-05-03 16:56:19 -0500

    CREAMOS LA CLASE ROCKET

--- sesion5.py
+++ sesion5.py
@@ -72,11 +72,7 @@ class Alien:
 
     def checkCollision(self, game):
         for rocket in game.rockets:
-            if( rocket.x < self.x + self.size and
-                rocket.x > self.x - self.size and
-                rocket.y < self.y + self.size and
-                rocket.y > self.y - self.size):
-
+            if( rocket.x < self.x + self.size and rocket.x > self.x - self.size and rocket.y < self.y + self.size and rocket.y > self.y - self.size):
                 game.rockets.remove(rocket)
                 game.aliens.remove(self)
 
@@ -91,3 +87,28 @@ class Hero:
     
     def draw(self):
         pygame.draw.rect(self.game.screen,(210, 250, 251),pygame.Rect(self.x, self.y, 8, 5))
+
+"""
+CREAMOS LA CLASE GENERATOR
+"""
+class Generator:
+    def __init__(self, game):
+        margin = 30
+        width = 50
+        for x in range(margin, game.width - margin, width):
+            for y in range(margin, int(game.height / 2), width):
+                game.aliens.append(Alien(game, x, y))
+
+"""
+CREAMOS LA CLASE ROCKET
+"""
+class Rocket:
+    def __init__(self, game, x, y):
+        self.x = x
+        self.game = game
+        self.y = y
+    
+    def draw(self):
+        pygame.draw.rect(self.game.screen,(254, 52, 110), pygame.Rect(self.x , self.y, 2, 4))
+
+        self.y -= 2
